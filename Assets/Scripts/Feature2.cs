9f4eb089ea6e80c9d9352a1dc11d05b4539a0e4a
HaiderRaza7 <Haider-raza77@hotmail.com>
2021-02-20 21:45:50 -0500

    Feature 2 99% completed

---
+++ Assets/Scripts/Feature2.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Feature2 : MonoBehaviour
+{
+    private int stage = 0;
+    public GameObject pointer;
+    public GameObject back;
+    public GameObject next;
+    public GameObject panel;
+    public GameObject minibox;
+    public GameObject[] textObjects;
+    void Start()
+    {
+        pointer.SetActive(false);
+        back.SetActive(true);
+        next.SetActive(true);
+
+        back.GetComponent<Button>().onClick.AddListener(BackButtonClicked);
+        next.GetComponent<Button>().onClick.AddListener(NextButtonClicked);
+
+        panel.SetActive(true);
+        minibox.SetActive(true);
+        foreach(GameObject textObject in textObjects){
+            textObject.SetActive(false);
+        }
+        textObjects[0].SetActive(true);
+    }
+
+    void NextButtonClicked(){
+        switch (stage)
+        {
+            case 0:
+                stage = 1;
+                textObjects[0].SetActive(false);
+                textObjects[1].SetActive(true);
+                break;
+            case 1:
+                stage = 2;
+                textObjects[1].SetActive(false);
+                // mini box and panel disable
+                panel.SetActive(false);
+                minibox.SetActive(false);
+                next.SetActive(false);
+                pointer.SetActive(true);
+                break;
+            case 2:
+                // Do nothing
+                break;
+            default:
+                // Incorrect stage value, set stage value back to 0
+                stage = 0;
+                break;
+        }
+    }
+
+    void BackButtonClicked(){
+        switch (stage)
+        {
+            case 0:
+                // Go to previous scene
+                // TODO: implement this!
+                SceneManager.LoadScene("SampleScene");
+                break;
+            case 1:
+                stage = 0;
+                textObjects[1].SetActive(false);
+                textObjects[0].SetActive(true);
+                break;
+            case 2:
+                stage = 1;
+                pointer.SetActive(false);
+                textObjects[1].SetActive(true);
+                panel.SetActive(true);
+                minibox.SetActive(true);
+                next.SetActive(true);
+                break;
+            default:
+                // Incorrect stage value, set stage value back to 0
+                stage = 0;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/TrackChargingStation.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TrackChargingStation : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private LineRenderer line;
+    private List<Vector3> point;
+
+    public Camera cam;
+    private NavMeshAgent agent;
+    private Transform agentTransform;
+    public GameObject[] chargingStations;
+    private float shortestDistance = 999999f;
+    private GameObject closestStation;
+    public float lineWidth = 0.04f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agentTransform = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody>();
+        line = GetComponent<LineRenderer>();
+        line.SetColors(Color.white, Color.white);
+        line.SetWidth(lineWidth, lineWidth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+            Debug.Log("Click");
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit)){
+                if(hit.collider.gameObject.tag == "Obstacle"){
+                    // Do nothing
+                    Debug.Log("Hit obstacle. Nothing happens");
+                } else {
+                    shortestDistance = 999999f;
+                    agentTransform.position = hit.point;
+                    gameObject.GetComponent<MeshRenderer>().enabled = true;
+                    // show the path finding to the nearest charging station
+                    int checker = 0;
+                    int cc = 0;
+                    foreach(GameObject station in chargingStations){
+                        float temp = Vector3.Distance(agentTransform.position, station.transform.position);
+                        if(shortestDistance > temp){
+                            shortestDistance = temp;
+                            closestStation = station;
+                            cc = checker;
+                        }
+                        checker += 1;
+                    }
+                    Debug.Log(cc);
+                    agent.SetDestination(closestStation.transform.position);
+                    // at this point in time, closestStation has the target location
+                    // at this point in time, agentTransform has the agent position.
+                    //agent.SetDestination(closestStation.position);
+                }
+            }
+        }
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (!agent.hasPath || agent.velocity.sqrMagnitude == 0f)
+                {
+                    // Pathfinding done: disable mesh for now
+                    gameObject.GetComponent<MeshRenderer>().enabled = false;
+                }
+            }
+        }
+        DisplayLineDestination();
+    }
+
+    private void DisplayLineDestination(){
+        if(agent.path.corners.Length < 2) return;
+
+        int i = 1;
+        while(i < agent.path.corners.Length){
+            line.positionCount = agent.path.corners.Length;
+            // point = agent.path.corners.ToList();
+            point = new List<Vector3>(agent.path.corners);
+            for(int j = 0; j < point.Count; j++){
+                line.SetPosition(j, point[j]);
+            }
+            i++;
+        }
+    }
+}
