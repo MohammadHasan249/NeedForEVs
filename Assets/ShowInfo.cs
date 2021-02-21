a61e9be8e4a9a1ee01e8e5e867f706bea149ce24
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-02-21 12:03:34 +0400

    added app 1 functionality

---
+++ Assets/ShowInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowInfo : MonoBehaviour
+{
+
+    [SerializeField] private GameObject canvas = null;
+    [SerializeField] private Camera camera = null;
+    [SerializeField] private GameObject[] objects = null;
+
+    public void OnMouseDown()
+    {
+        foreach (GameObject obj in objects)
+        {
+            obj.SetActive(false);
+        }
+        canvas.SetActive(true);
+        camera.transform.position = new Vector3(2.7f, 6.1f, -385f);
+        camera.transform.rotation = Quaternion.identity;
+    }
+
+}
