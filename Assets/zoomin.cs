14c8bae2ca0fb001e899486355f193e02819c8bd
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-02-21 04:21:52 +0400

    Adding in zooming

---
+++ Assets/zoomin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoomin : MonoBehaviour
+{
+    [SerializeField] private GameObject startBtn = null;
+
+    private bool isZooming = false;
+
+    private void Update()
+    {
+
+        while (isZooming)
+        {
+
+        }
+    }
+
+}
