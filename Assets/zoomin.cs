dfd458e8a45e8fa199b199a9731cd51eb443fa1d
Mohammad Hasan <mohd.hasan@mail.utoronto.ca>
2021-02-21 04:59:58 +0400

    zoom functionality added

--- Assets/zoomin.cs
+++ Assets/zoomin.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class zoomin : MonoBehaviour
 {
-    [SerializeField] private GameObject startBtn = null;
 
-    private bool isZooming = false;
+    [SerializeField] private Transform startMarker = null;
+    [SerializeField] private Transform endMarker = null;
+    [SerializeField] private float speed = 1.8f;
+
+    private float startTime;
+    private float journeyLength;
+
+    private void Start()
+    {
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(startMarker.position, endMarker.position);
+    }
 
     private void Update()
     {
+        float distCovered = (Time.time - startTime) * speed;
+        float fractionOfJourney = distCovered / journeyLength;
 
-        while (isZooming)
-        {
-
-        }
+        transform.position = Vector3.Lerp(startMarker.position, endMarker.position, fractionOfJourney);
     }
 
 }
